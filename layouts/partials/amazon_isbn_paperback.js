073d9d62dc1fbe95af60082a65f557f7b328f3d1
Bud Parr <budparr@gmail.com>
2017-06-23 23:20:21 -0400

    bring over

---
+++ layouts/partials/amazon_isbn_paperback.js
@@ -0,0 +1,26 @@
+<script type="text/javascript">
+  function ISBN13toISBN10(isbn13) {
+    var start = isbn13.substring(3, 12);
+    var sum = 0;
+    var mul = 10;
+    var i;
+    for (i = 0; i < 9; i++) {
+      sum = sum + (mul * parseInt(start[i]));
+      mul -= 1;
+    }
+    var checkDig = 11 - (sum % 11);
+    if (checkDig == 10) {
+      checkDig = "X";
+    } else if (checkDig == 11) {
+      checkDig = "0";
+    }
+    return start + checkDig;
+  }
+  var switchIsbn = function(el) {
+    el.innerHTML = '<a href="https://www.amazon.com/dp/' + ISBN13toISBN10("{{ .paperback | safeJS }}") + '" title="Amazon" target="_blank" class="{{ .buylinkClasses }}">Amazon</a>';
+  }
+  var isbn10 = document.getElementsByClassName("isbn10_{{ .paperback | safeJS }}");
+  for(var i = 0; i < isbn10.length; i++) {
+    switchIsbn(isbn10[i])
+  }
+</script>
